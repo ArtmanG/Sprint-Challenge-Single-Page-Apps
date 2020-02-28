26a225f394179e0c88df4867b42049dd07f04a1c
Gregory Hawman <g.hawman@gmail.com>
2020-02-28 09:32:48 -0800

    fixed my quotation marks

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React from "react";
+import React from 'react';
 import { Route } from 'react-router-dom'
 
-import Header from "./components/Header.js";
+import Header from './components/Header.js';
 import WelcomePage from './components/WelcomePage';
 import CharacterList from './components/CharacterList';
-import Navigation from "./components/Navigation.js";
+import Navigation from './components/Navigation.js';
 
 
 function App() {
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,6 +1,8 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react';
 import Axios from 'axios';
-import CharacterCard from './CharacterCard'
+
+import CharacterCard from './CharacterCard';
+import SearchForm from './SearchForm';
 
 function CharacterList() {
   const [characters, setCharacters] = useState([]);
@@ -15,22 +17,24 @@ function CharacterList() {
       .catch(error => {
         console.log('Error:', error);
       });
+
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
   }, []);
 
   return (
-    <section className="character-list">
+    <section className='character-list'>
+    <SearchForm />
       {characters.map((character, index) => {
-        return (
-          <CharacterCard
-            key={index}
-            name={character.name}
-            status={character.status}
-            species={character.species}
-            gender={character.gender}
-          />
-        );
+          return (
+            <CharacterCard
+              key={index}
+              name={character.name}
+              status={character.status}
+              species={character.species}
+              gender={character.gender}
+            />
+          );
       })};
     </section>
   );
